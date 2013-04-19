079186135f03aeb9b3c61bdb03acb128e62640ce
DianaAnisutina <freilina_07@mail.ru>
2013-04-20 02:58:24 +0700

    added [recvpost] package
    post can be received using pop3
    mostly works on correct data
    you can input your post-server, login, password, and number of the
    letter

---
+++ SelfCounter/src/ru/nsu/fit/anisutina/recvpost/Pop3PostReceiver.java
@@ -0,0 +1,96 @@
+package ru.nsu.fit.anisutina.recvpost;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.CharBuffer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Diana
+ * Date: 19.04.13
+ * Time: 22:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class Pop3PostReceiver {
+    private static String pop3Server = "ccfit.nsu.ru";
+    private static Socket socket = null;
+    private final static int PORT = 110;
+    private static BufferedReader inputStream = null;
+    private static BufferedReader inReader = null;
+    private static OutputStreamWriter fileOutputStream = null;
+    private static String username = "anisyutina";
+    private static String password = "12345";
+    private static String num_of_letter = "222";
+    private static final int BUFFERSIZE = 1024;
+
+    public static void main(String args[]) {
+        if(args.length > 0) {
+            pop3Server = args[0];
+        }
+        try {
+            socket = new Socket(pop3Server, PORT);
+            inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String response = inputStream.readLine();
+            System.out.println(response);
+
+            inReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter your username:");
+            username = inReader.readLine();
+
+            String COMMAND = "USER " + username + "\r\n";
+            socket.getOutputStream().write(COMMAND.getBytes());
+
+            response = inputStream.readLine();
+            System.out.println(response);
+
+            System.out.println("Enter your password:");
+            password = inReader.readLine();
+            COMMAND = "PASS " + password + "\r\n";
+            socket.getOutputStream().write(COMMAND.getBytes());
+
+            response = inputStream.readLine();
+            System.out.println(response);
+
+            System.out.println("Enter number of letter:");
+            num_of_letter = inReader.readLine();
+            COMMAND = "RETR " + num_of_letter + "\r\n";
+            File letter = new File("letter_" + num_of_letter + ".txt");
+            socket.getOutputStream().write(COMMAND.getBytes());
+            char[] message_buf = new char[BUFFERSIZE];
+
+            if(!letter.exists()) {
+                fileOutputStream = new OutputStreamWriter(new FileOutputStream(letter));
+                int num = inputStream.read(message_buf);
+                fileOutputStream.write(message_buf);
+                while (BUFFERSIZE == num) {
+                    num = inputStream.read(message_buf);
+                    if(num > 0) {   fileOutputStream.write(message_buf, 0, num);    }
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Socket was not created [main]:Pop3PostReceiver");
+        }
+        finally {
+            if(inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {   System.err.println("input socket stream was not closed [main]:Pop3PostReceiver");    }
+            }
+            if(inReader != null) {
+                try {
+                    inReader.close();
+                } catch (IOException e) {   System.err.println("input std stream was not closed [main]:Pop3PostReceiver");    }
+            }
+            if(fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {   System.err.println("input file stream was not closed [main]:Pop3PostReceiver");    }
+            }
+            if(socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {   System.err.println("socket was not closed [main]:Pop3PostReceiver");   }
+            }
+        }
+    }
+}
