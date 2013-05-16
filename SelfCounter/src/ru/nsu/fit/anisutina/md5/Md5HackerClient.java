28a15f4783bcb9783159d2c1bee62db9523b23d5
DianaAnisutina <freilina_07@mail.ru>
2013-05-17 00:38:19 +0700

    client do right work and correct
    go over the own interval
    interpretation number to acgt
    encode by md5 and compare with expected result

--- SelfCounter/src/ru/nsu/fit/anisutina/md5/Md5HackerClient.java
+++ SelfCounter/src/ru/nsu/fit/anisutina/md5/Md5HackerClient.java
@@ -1,9 +1,6 @@
 package ru.nsu.fit.anisutina.md5;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -21,9 +18,29 @@ public class Md5HackerClient {
     private static int PORT = 5500;
     private static InetAddress IP = null;
     private static Socket socket = null;
-    private static InputStream inputStream = null;
-    private static OutputStream outputStream = null;
-    private static Integer lengthInteval = 20;
+    private static BufferedReader inputStream = null;
+    private static BufferedWriter outputStream = null;
+    private static final char[] ACGT = {'A', 'C', 'G', 'T'};
+    private static String expected_result = null;
+    private static Integer start = null;
+    private static Integer end = null;
+    private static String original_string = null;
+
+    private static String encodeStringByMd5(String to_encode) {
+        MessageDigest md = null;
+        StringBuffer sb = new StringBuffer();
+        try {
+            md = MessageDigest.getInstance("MD5");
+            md.update(to_encode.getBytes());
+            byte byteData[] = md.digest();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("did not get MessageDigest [encodeStringByMd5]:Md5HackerClient");
+        }
+        return sb.toString();
+    }
 
     public static void main(String args[]) {
         if(args.length > 0){
@@ -31,36 +48,64 @@ public class Md5HackerClient {
             try {
                 IP = InetAddress.getByName(args[0]);
             } catch (UnknownHostException e) {
-                System.err.println("did not convert from string to InetAddress [main]:DataTransmissionClient");
+                System.err.println("did not convert from string to InetAddress [main]:Md5HackerClient");
             }
         } else {
             try {
-                IP = InetAddress.getByName("192.168.1.4");
+                IP = InetAddress.getByName("192.168.1.10");
             } catch (UnknownHostException e) {
-                System.err.println("did not convert from string to InetAddress [main]:RateClient");
+                System.err.println("did not convert from string to InetAddress [main]:Md5HackerClient");
             }
         }
 
         try {
+            /*
             socket = new Socket(IP, PORT);
-            inputStream = socket.getInputStream();
-            outputStream = socket.getOutputStream();
-            byte[] byte_buffer = new byte[100];
-            int num = inputStream.read(byte_buffer);
-            Integer start = new Integer(byte_buffer[0]);
-            Integer current = start;
-            Integer last = start + lengthInteval;
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            do {
-                md.update(current.byteValue());
-                System.out.println("current:" + current.byteValue());
-                outputStream.write(md.digest());
-                current++;
-            } while (!last.equals(current));
+            inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //outputStream = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            String task_line = inputStream.readLine();
+            inputStream.close();
+            socket.close();
+              */
+            String task_line = "10 20 cd6a9bd2a175104eed40f0d33a8b4020";
+            String[] parameters = task_line.split(" ");
+            start = new Integer(parameters[0]);
+            end = new Integer(parameters[1]);
+            expected_result = parameters[2];
+            int devided4 = 0;
+            int multed4 = 0;
+            int rest = 0;
+            //main loop
+            for(int current_orig = start; current_orig <= end; current_orig++) {
+                StringBuilder construct_code = new StringBuilder();
+                int current = current_orig;
+                do{
+                    devided4 = (current >> 2);
+
+                    multed4 = (devided4 << 2);
+                    rest = current - multed4;
+                    construct_code.append(ACGT[rest]);
+                    current = devided4;
+
+                } while (current != 0);
+                String decoded_string = construct_code.reverse().toString();
+                String coded_string = encodeStringByMd5(decoded_string);
+                if(expected_result.equals(coded_string)) {
+                    original_string = decoded_string;
+                    break;
+                }
+            }
+            System.out.println(original_string + " was encoded to " + expected_result);
+               /*
+            if(original_string != null) {
+                socket = new Socket(IP, PORT);
+                outputStream = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                outputStream.write(original_string);
+                outputStream.close();
+                socket.close();
+            }
         } catch (IOException e) {
-            System.err.println("socket was not created");
-        } catch (NoSuchAlgorithmException e) {
-            System.err.println("did not create messge digest[main]:Md5HackerClient");
+            System.err.println("socket was not created");*/
         }
         finally {
             if(inputStream != null) {
